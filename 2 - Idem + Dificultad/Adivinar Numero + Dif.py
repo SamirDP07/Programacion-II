d759953d39aa3c5a88134f461e425ee0f2bc2087
BuildTools <unconfigured@null.spigotmc.org>
2023-11-04 22:13:54 -0300

    NOSE

--- 2 - Idem + Dificultad/Adivinar Numero + Dif.py
+++ 2 - Idem + Dificultad/Adivinar Numero + Dif.py
@@ -4,24 +4,48 @@
 #-hay que validar la entrada
 #-re
 import random
-jugar =1
+jugar =True
 dificultad = 1
 ingresoValido=True
-while jugar ==1:
+jugarT=""
+#^jugar Texto
+while jugar ==True:
     print("A continucacion ingrese la dificultad en la que desea jugar")
-    print("[1]-Facil")
-    print("[2]-Dificil")
+    print("[1]-Facil (Del 1 al 20)")
+    print("[2]-Dificil (Del  1 al 200)")
     dificultad = int(input("-->"))
     if dificultad == 1:
+        print("[Dificultad - Facil]")
         rangoMax = 20
         intentos = 6
     elif dificultad == 2:
+        print("[Dificultad - Dificil]")
         rangoMax = 200
         intentos = 7
     else:
         ingresoValido=False
         print("No se ingreso una dificultad valida")
-        print("Desea intentar de nuevo? y/n")
-        jugar = input("-->").capitalize
-        
-        
+    if ingresoValido==True:
+        Nrandom=random.randint(1,rangoMax)
+        for i in range(1,intentos,1):
+            print("Quedan [" , (intentos-i) , "] intentos")
+            print("Ingrese un numero a probar")
+            intento = int(input("-->"))
+            if intento == Nrandom:
+                print("Adivino!")
+                break
+            elif intento>Nrandom:
+                print("[Intento Fallido]")
+                print("El numero ingresado es mayor al numero a adivinar")
+            else:
+                print("[Intento Fallido]")
+                print("El numero ingresado es menor al numero a adivinar")
+                
+            print("#######################################")
+    print("Desea intentar de nuevo?Y/N")
+    jugarT=input("-->")
+    jugarT=jugarT.capitalize
+    if jugarT == "Y":
+        jugar=True
+    else:
+        jugar=False
